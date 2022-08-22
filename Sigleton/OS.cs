bf977933addd270e71b7a96022ef12e960d50dd5
Vladislav83530 <88848593+Vladislav83530@users.noreply.github.com>
2022-08-22 20:49:34 +0300

    Singleton
    Singleton

---
+++ Sigleton/OS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigleton
+{
+    internal class OS
+    {
+        private static OS instance;
+        private static object syncRoot = new Object();
+
+        public string Name { get; private set; }
+
+        protected OS(string name)
+        {
+            Name = name;    
+        }
+
+        public static OS GetInstance(string name)
+        {
+            if(instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new OS(name);
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ Sigleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using Sigleton;
+
+//Computer comp = new Computer();
+//comp.Launch("Windows 8.1");
+//Console.WriteLine(comp.OS.Name);
+
+//comp.OS = OS.GetInstance("Windows 11");
+//Console.WriteLine(comp.OS.Name);
+
+(new Thread(() =>
+{
+    Computer comp2 = new Computer();
+    comp2.OS = OS.GetInstance("Windows 11");
+    Console.WriteLine(comp2.OS.Name);
+})).Start();
+
+Computer comp = new Computer();
+comp.Launch("Windows 8.1");
+Console.WriteLine(comp.OS.Name);
